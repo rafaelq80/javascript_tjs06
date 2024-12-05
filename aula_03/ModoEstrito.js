1c61454439ef4c08c952a2062d3a91ad3fd1c5fd
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2024-12-05 15:45:28 -0300

    Aula 04

---
+++ aula_03/ModoEstrito.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let numero = 10;
+
+console.log("O Valor da Variável numero é: ", numero);
+
+numero = 100;
+
+console.log("O Valor da Variável numero é: ", numero);
---
+++ aula_04/Tabuada.js
@@ -0,0 +1,8 @@
+const leia = require('readline-sync')
+
+let numero = leia.questionInt('Digite um numero: ');
+
+for(let contador = 1; contador <= 10; contador++){
+    console.log(`${numero} x ${contador} = ${numero * contador}`);
+}
+
